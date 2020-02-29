a8339129dac7b9f23117f08c65615b65bdc63339
春末丶冬初 <48005341+kingchun@users.noreply.github.com>
2020-02-29 16:54:21 +0800

    登录界面
    PHP以及登录成功全部都没写

---
+++ conn.php
@@ -0,0 +1,5 @@
+<?php   
+ $conn = mysql_connect("149.129.107.38","ceshi","") or die("数据库链接错误".mysql_error());  
+ mysql_select_db("info_db",$conn) or die("数据库访问错误".mysql_error());  
+ mysql_query("set names gb2312");  
+?>  
